6559376fbd64d3cf0ed0a2253cb18cad1645f9a2
Alex Muniz <munizalex45@gmail.com>
2023-07-21 13:21:28 -0300

    Admin

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>I'am Admin</span>
+      <span>© VirgilHawkins Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
